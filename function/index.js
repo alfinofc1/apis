7967c4c8a58633c95100a37b822371a16c459217
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-02-10 00:42:33 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -3,4 +3,5 @@ module.exports = {
 	smartContract: require('./scraper/smartContract'),
 	ragBot: require('./scraper/ragBot'),
 	degreeGuru: require('./scraper/degreeGuru')
+	YtMp3: require('./scraper/youtube.js')
 }
